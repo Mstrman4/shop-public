42a0e71a9b831094116e619b5544e6ba0804fdbe
Mstrman4 <daramade4@gmail.com>
2024-10-07 02:16:08 +0330

    یی

--- js/app.js
+++ js/app.js
@@ -68,116 +68,103 @@ let categories = await getcat();
 let singelprod = await getprod(108);
 let allprod = await getallprod();
 
-// // console.log(allprod[0]);
+// console.log(allprod[0]);
 
 
-// categories.forEach(e => {
+categories.forEach(e => {
 
-//     let aatg = document.createElement("a");
-//     aatg.setAttribute("href",`/category.html?catg=${e.name}`)
-//     aatg.className="category-box bg-white border border-gray-300 p-4 text-green-900 rounded-lg shadow-md  text-center"
+    let aatg = document.createElement("a");
+    aatg.setAttribute("href",`/category.html?catg=${e.name}`)
+    aatg.className="category-box bg-white border border-gray-300 p-4 text-green-900 rounded-lg shadow-md  text-center"
  
     
 
     
-//     let categoryElement = document.createElement("fetch-catg");
-//     // categoryElement.insertAdjacentHTML("afterbegin", `<a slot="atg" href="/category.html?catg=${e.name}"></a>`)
-//     categoryElement.insertAdjacentHTML("afterbegin", `   <p slot="catg" class="font-semibold text-center text-black">${e.name}</p>`)
-//     aatg.appendChild(categoryElement);
-//     $.querySelector(".feach-catg").appendChild(aatg);
+    let categoryElement = document.createElement("fetch-catg");
+    // categoryElement.insertAdjacentHTML("afterbegin", `<a slot="atg" href="/category.html?catg=${e.name}"></a>`)
+    categoryElement.insertAdjacentHTML("afterbegin", `   <p slot="catg" class="font-semibold text-center text-black">${e.name}</p>`)
+    aatg.appendChild(categoryElement);
+    $.querySelector(".feach-catg").appendChild(aatg);
 
 
-// });
+});
 
 
 
 
-// let indexFEali = 0
-// train(allprod)
+let indexFEali = 0
+train(allprod)
 
-// function train (prods) {
+function train (prods) {
     
-//     for (let index = 0; index < 8; index++) {
+    for (let index = 0; index < 8; index++) {
         
-//        if (prods[indexFEali]==undefined) {
-//         $.querySelector("#loadMore").style.display="none"
-//        }
+       if (prods[indexFEali]==undefined) {
+        $.querySelector("#loadMore").style.display="none"
+       }
        
-//         let amc = document.createElement("a")
-//         amc.setAttribute("href",`prod.html?id=${prods[indexFEali].id}`)
-//         amc.className="w-6/12 md:w-3/12 lg:w-3/12 xl:w-2/12 p-4 "
+        let amc = document.createElement("a")
+        amc.setAttribute("href",`prod.html?id=${prods[indexFEali].id}`)
+        amc.className="w-6/12 md:w-3/12 lg:w-3/12 xl:w-2/12 p-4 "
 
-//         let mc = document.createElement("m-c");
+        let mc = document.createElement("m-c");
         
         
-//         mc.insertAdjacentHTML("beforeend", ``);
+        mc.insertAdjacentHTML("beforeend", ``);
         
-//         mc.insertAdjacentHTML("beforeend", `<img slot="mc-img" class="w-full max-h-[129px] md:max-h-[152px] lg:md:max-h-[218px]"  src="${prods[indexFEali].images[0]}" alt="">`);
-//         mc.insertAdjacentHTML("beforeend", `<p slot="mc-name" class="font-bold">${prods[indexFEali].title}</p>`);
-//         mc.insertAdjacentHTML("beforeend", `<span slot="mc-price" class="font-bold">${prods[indexFEali].price}$</span>`);
-//         mc.insertAdjacentHTML("beforeend", `<span slot="mc-rate" class="font-normal">${prods[indexFEali].rating}</span>`);
-//         mc.insertAdjacentHTML("beforeend", `<span slot="mc-stock" class="font-normal">${prods[indexFEali].stock} sold</span>`);
+        mc.insertAdjacentHTML("beforeend", `<img slot="mc-img" class="w-full max-h-[129px] md:max-h-[152px] lg:md:max-h-[218px]"  src="${prods[indexFEali].images[0]}" alt="">`);
+        mc.insertAdjacentHTML("beforeend", `<p slot="mc-name" class="font-bold">${prods[indexFEali].title}</p>`);
+        mc.insertAdjacentHTML("beforeend", `<span slot="mc-price" class="font-bold">${prods[indexFEali].price}$</span>`);
+        mc.insertAdjacentHTML("beforeend", `<span slot="mc-rate" class="font-normal">${prods[indexFEali].rating}</span>`);
+        mc.insertAdjacentHTML("beforeend", `<span slot="mc-stock" class="font-normal">${prods[indexFEali].stock} sold</span>`);
    
         
-//         amc.appendChild(mc);
-//         document.querySelector(".main-content").appendChild(amc);
+        amc.appendChild(mc);
+        document.querySelector(".main-content").appendChild(amc);
     
         
-//         indexFEali++;        
-//     }
-// }
-
-// $.querySelector("#loadMore").addEventListener("click", ()=>{
-//     train(allprod)
-// })
-
-
-// function banner (){
-
-//     let bnas = document.createElement("div")
-
-//     bnas.className="flex  flex-col gap-6 mt-10 p-3 justify-center md:w-6/12"
-
-
-//     bnas.insertAdjacentHTML("beforeend",`
-         
-//             <h1 class="text-left text-3xl font-extrabold">${singelprod.title}</h1>
-//             <p class="text-slate-500 ">${singelprod.description}</p>
-//             <div class="flex ">          <button type="button" class="md:w-5/12 mx-1    text-white w-full bg-[#1E4C2F]  font-medium rounded-lg text-2xl font-extrabold px-5 py-2.5 mb-2 ">buy ${singelprod.price}</button>
-//             <button type="button" class=" md:w-5/12 mx-1   w-full text-[#1E4C2F]  hover:text-white border border-[#1E4C2F] hover:bg-[#5d8b6e]  font-medium rounded-lg text-2xl px-5 py-2.5 text-center mb-2">view detail</button>
-//            </div>
-//        `)
-    
-//         $.querySelector("#promoBanner").appendChild(bnas)
-// }
-// banner()
-
-
-
-
-
-
-
-
-
-
-
-
-
-async function test (){
-
-    let data1 = await fetch("https://laser.ir/modules/servers/product/views/viewpanel.php?id=10773&u=client")
-
-    console.log(data1);
-
-    let data2 = data1.json()
-
-    console.log(data2);
-    
-    
+        indexFEali++;        
+    }
 }
 
-test()
+$.querySelector("#loadMore").addEventListener("click", ()=>{
+    train(allprod)
+})
+
+
+function banner (){
+
+    let bnas = document.createElement("div")
+
+    bnas.className="flex  flex-col gap-6 mt-10 p-3 justify-center md:w-6/12"
+
+
+    bnas.insertAdjacentHTML("beforeend",`
+         
+            <h1 class="text-left text-3xl font-extrabold">${singelprod.title}</h1>
+            <p class="text-slate-500 ">${singelprod.description}</p>
+            <div class="flex ">          <button type="button" class="md:w-5/12 mx-1    text-white w-full bg-[#1E4C2F]  font-medium rounded-lg text-2xl font-extrabold px-5 py-2.5 mb-2 ">buy ${singelprod.price}</button>
+            <button type="button" class=" md:w-5/12 mx-1   w-full text-[#1E4C2F]  hover:text-white border border-[#1E4C2F] hover:bg-[#5d8b6e]  font-medium rounded-lg text-2xl px-5 py-2.5 text-center mb-2">view detail</button>
+           </div>
+       `)
+    
+        $.querySelector("#promoBanner").appendChild(bnas)
+}
+banner()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
